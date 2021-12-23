73bdf510f7363d86e6ed5552f69ff4e051263b49
shimozuki <robbishimozuki@gmail.com>
2021-12-23 15:43:52 +0800

    update

--- app/Controllers/Pesanan.php
+++ app/Controllers/Pesanan.php
@@ -27,17 +27,16 @@ class Pesanan extends ResourceController
             $model = new PesananModel();
 
             $query = $model->query("SELECT t_penjualan.no_transaksi, t_pengiriman.no_penjualan, t_pengiriman.nama_tujuan, 
-            t_driver.kode_pin, m_user_company.company_id, m_driver.nama_depan, m_driver.hp1, t_penjualan_detail.*
+            t_driver.kode_pin, m_user_company.company_id, m_driver.nama_depan, m_driver.hp1, t_penjualan.id
             FROM t_pengiriman 
             INNER JOIN t_penjualan ON t_pengiriman.no_resi= t_penjualan.no_transaksi
             INNER JOIN t_driver ON t_pengiriman.id_driver = t_driver.kd_driver
             INNER JOIN m_driver ON t_driver.kd_driver = m_driver.kd_driver
             INNER JOIN m_user_company ON t_penjualan.user_id_toko = m_user_company.id
-            INNER JOIN t_penjualan_detail ON t_penjualan.no_transaksi= t_penjualan_detail.no_transaksi
             WHERE t_penjualan.user_id_toko = 10 AND date(t_penjualan.tanggal) BETWEEN '2021/12/20' AND '2021/12/23' 
-            AND t_penjualan.status_barang = 4 ");
+            AND t_penjualan.status_barang = 4 ")->getRow();
 
-            if(empty($query->getResult())){
+            if (empty($query)) {
                 $respond = [
                     'status' => 500,
                     'error' => true,
@@ -45,16 +44,37 @@ class Pesanan extends ResourceController
                     'data' => []
                 ];
                 return $this->respondCreated($respond);
-            }else{
-                $respond = [
-                    'status' => 200,
-                    'error' => true,
-                    'messages' => "Success",
-                    'data' => $query->getResult()
-                 ];
-            return $this->respondCreated($respond);
-          
+            } else {
+                $no_transaksi = $query->no_transaksi;
+
+                if (empty($no_transaksi)) {
+                    $respond = [
+                        'status' => 500,
+                        'error' => true,
+                        'messages' => "Sorry Not Found ",
+                        'data' => []
+                    ];
+                    return $this->respondCreated($respond);
+                } else {
+                    $pesanan = $model->query("SELECT * FROM t_penjualan_detail WHERE no_transaksi = '$no_transaksi'");
+
+                    $respond = [
+                        'data' => [
+                           "no_transaksi" => $no_transaksi,
+                           "pembeli"      => $query->nama_tujuan,
+                           "pesanan"      => ($pesanan->getResult()),
+                           "id_order"     => $query->id,
+                           "pin"          => $query->kode_pin,
+                           "noHp"         => $query->hp1,
+                           "comp_id"      => $query->company_id,
+                           "nama_driver"  => $query->nama_depan,
+
+                        ]
+                    ];
+                return $this->respondCreated($respond);
+              
+                }
+            }
         }
-    }
     }
 }
