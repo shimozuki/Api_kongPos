f5a178fcd1a7f3d23a0d37304358fff0b5681e51
shimozuki <robbishimozuki@gmail.com>
2021-12-24 12:12:23 +0800

    update

--- app/Controllers/Pesanan.php
+++ app/Controllers/Pesanan.php
@@ -10,9 +10,10 @@ class Pesanan extends ResourceController
 {
     public function transaksi()
     {
+        date_default_timezone_set("Asia/Jakarta");
         $companyid = $this->request->getVar('company_id');
-        $startdate = $this->request->getVar('startdate');
-        $endate = $this->request->getVar('enddate');
+        $startdate = date('Y-m-d', strtotime("-1 day", strtotime(date("Y-m-d"))));
+        $endate = date("Y-m-d");
 
         if(empty($companyid))
         {
@@ -33,8 +34,8 @@ class Pesanan extends ResourceController
             INNER JOIN t_driver ON t_pengiriman.id_driver = t_driver.kd_driver
             INNER JOIN m_driver ON t_driver.kd_driver = m_driver.kd_driver
             INNER JOIN m_user_company ON t_penjualan.user_id_toko = m_user_company.id
-            WHERE t_penjualan.user_id_toko = 10 AND date(t_penjualan.tanggal) BETWEEN '2021/12/20' AND '2021/12/23' 
-            AND t_penjualan.status_barang = 4 ")->getRow();
+            WHERE t_penjualan.user_id_toko = $companyid AND date(t_penjualan.tanggal) BETWEEN '$startdate' AND '$endate'
+            AND t_penjualan.status_barang = 3 ")->getRow();
 
             if (empty($query)) {
                 $respond = [
